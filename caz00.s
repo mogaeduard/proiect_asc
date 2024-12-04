9a6673dac0aaf4050e16f54f7b1af53346a2102a
mogaeduard <eduard-andrei.moga@s.unibuc.ro>
2024-12-04 19:26:49 +0200

    ADD aproape terminat

--- caz00.s
+++ caz00.s
@@ -4,7 +4,16 @@
     comanda: .space 4
     formatScanf: .asciz "%d"
     afisareADD: .asciz "%d: (%d, %d)\n"
-    comenziExecutate: .space 4
+    comenziParsate: .long 0
+    nrComenziAdd: .space 4
+    nrComenziAddExecutate: .long 0
+    idFisier: .space 4
+    dimensiuneFisier: .space 4
+    primulZero: .space 4
+    sfarsitInterval: .space 4
+    pozitieZero: .space 4
+    ct: .space 4
+    msjEroare: .asciz "(0, 0)\n"
 
 .text
 .global main
@@ -14,7 +23,7 @@ main:
     jmp zero
 
 zero:
-    cmp $1024, %ecx
+    cmp $1000, %ecx
     jge citireNrComenzi
     movb $0, (%edi, %ecx)
     inc %ecx
@@ -29,30 +38,140 @@ citireNrComenzi:
     jmp parsareComenzi
 
 parsareComenzi:
+    movl comenziParsate, %ecx                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
     cmpl %ecx, nrComenzi
     je et_exit
     inc %ecx
+    movl %ecx, comenziParsate
     xor %edx, %edx
-
-    push %ecx
     push $comanda
     push $formatScanf
     call scanf
     add $8, %esp
-    pop %ecx
-
     movl comanda, %edx
     cmp $1, %edx
-    je ADD
+    je citireNrComenziADD
     cmp $2, %edx
     je GET
     cmp $3, %edx
     je REMOVE
     jmp DEFRAG
 
-ADD:
-    jmp parsareComenzi
+citireNrComenziADD:
+    push $nrComenziAdd
+    push $formatScanf
+    call scanf
+    add $8, %esp
+    jmp ADD  
 
+ADD:
+    movl nrComenziAddExecutate, %ecx
+    cmpl %ecx, nrComenziAdd
+    je parsareComenzi
+    inc %ecx
+    movl %ecx, nrComenziAddExecutate
+
+    push $idFisier
+    push $formatScanf
+    call scanf
+    add $8, %esp
+    push $dimensiuneFisier
+    push $formatScanf
+    call scanf
+    add $8, %esp
+
+    jmp blocuriNecesare
+    
+blocuriNecesare:
+    xor %ebx, %ebx
+    xor %edx, %edx
+    mov dimensiuneFisier, %eax
+    mov $8, %ebx
+    div %ebx
+    cmp $0, %edx
+    je rest0
+    jg restMare
+
+rest0:
+    mov %eax, dimensiuneFisier
+
+    xor %ebx, %ebx
+    xor %edx, %edx
+    xor %ecx, %ecx
+    jmp cautSpatiuLiber
+restMare:
+    add $1, %eax
+    mov %eax, dimensiuneFisier
+    xor %ebx, %ebx
+    xor %edx, %edx
+    xor %ecx, %ecx
+    jmp cautSpatiuLiber
+
+
+cautSpatiuLiber:
+    mov $0, %eax
+    cmpb (%edi, %ecx), %al
+    je retinemPrimulZero    
+    inc %ecx
+    jmp cautSpatiuLiber
+
+retinemPrimulZero:
+    movl %ecx, primulZero 
+    inc %ecx
+    jmp ZeroDisponibil
+
+ZeroDisponibil:
+    xor %eax, %eax
+    cmpb (%edi, %ecx) , %al
+    jne verificareADD
+    movl %ecx, ct
+    inc %ecx
+    jmp ZeroDisponibil
+   
+verificareADD:
+    movl ct, %ebx
+    sub primulZero, %ebx
+    cmpl dimensiuneFisier, %ebx
+    jne adaugareInMemorie             
+    jmp cautSpatiuLiber
+
+
+adaugareInMemorie:
+    cmp $1000, %ecx
+    je ADD_eroare
+    
+    movl primulZero, %ecx
+    mov dimensiuneFisier, %edx
+    addl primulZero, %edx
+    sub $1, %edx
+    movl %edx, sfarsitInterval
+    xor %edx, %edx
+    mov idFisier, %eax
+    jmp adaugareInMemorieContinuare
+
+adaugareInMemorieContinuare:
+    movb %al, (%edi,%ecx)
+    cmpl %ecx, sfarsitInterval
+    je et_afisareADD
+    inc %ecx
+    cmp $1000, %ecx
+    je ADD_eroare
+    jmp adaugareInMemorieContinuare
+
+ADD_eroare:
+    push msjEroare
+    call printf
+    add $4, %esp
+    jmp ADD
+
+et_afisareADD:
+    push sfarsitInterval
+    push primulZero
+    push idFisier
+    push $afisareADD
+    call printf
+    add $16, %esp
+    jmp ADD
 GET:
     jmp parsareComenzi
 
